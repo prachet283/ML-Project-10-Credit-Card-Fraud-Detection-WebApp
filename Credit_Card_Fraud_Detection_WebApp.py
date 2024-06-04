9f73fb9fa1e8cf6abeaa3a0a1e01f4dbbc7280ba
Prachet Pandav <113706290+prachet283@users.noreply.github.com>
2024-06-04 16:53:19 +0530

    Update Credit_Card_Fraud_Detection_WebApp.py

--- Credit_Card_Fraud_Detection_WebApp.py
+++ Credit_Card_Fraud_Detection_WebApp.py
@@ -10,7 +10,7 @@ import pickle
 import streamlit as st
 
 #loading. the saved model
-loaded_model = pickle.load(open('C:/Users/prachet/OneDrive - Vidyalankar Institute of Technology/Desktop/Coding/Machine Learning/ML-Project-10-Credit Card Fraud Detection/creditcard_fraud_trained_model.sav','rb'))
+loaded_model = pickle.load(open('creditcard_fraud_trained_model.sav','rb'))
 
 #creating a function for prediction
 
